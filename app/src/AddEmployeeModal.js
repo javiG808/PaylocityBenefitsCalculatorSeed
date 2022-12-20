20ed92ce613a726d25a2d84ff4f10976c1d20cea
Javier Grijalva <javi.grijalva@gmail.com>
2022-12-19 22:31:02 -0600

    error handling

--- app/src/AddEmployeeModal.js
+++ app/src/AddEmployeeModal.js
@@ -19,6 +19,53 @@ const initialValues = {
 
 const AddEmployeeModal = (props) => {   
     const [values, setValues] = useState(initialValues); 
+    const [errors, setErrors] = useState([]);
+
+    let handleValidation(){
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
+    
+        //Name
+        if(!fields["name"]){
+          formIsValid = false;
+          errors["name"] = "Cannot be empty";
+        }
+    
+        if(typeof fields["name"] !== "undefined"){
+          if(!fields["name"].match(/^[a-zA-Z]+$/)){
+            formIsValid = false;
+            errors["name"] = "Only letters";
+          }      	
+        }
+    
+        //Email
+        if(!fields["email"]){
+          formIsValid = false;
+          errors["email"] = "Cannot be empty";
+        }
+    
+        if(typeof fields["email"] !== "undefined"){
+          let lastAtPos = fields["email"].lastIndexOf('@');
+          let lastDotPos = fields["email"].lastIndexOf('.');
+    
+          if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+            formIsValid = false;
+            errors["email"] = "Email is not valid";
+          }
+        }
+    
+    
+    
+        this.setState({errors: errors});
+        return formIsValid;
+      }
+
+      if(this.handleValidation()){
+        alert("Form submitted");
+      }else{
+        alert("Form has errors.")
+      }
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
